726167955e0f37e43cd160a8be76df9039bc3d3f
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-21 12:39:44 +0100

    new commit

---
+++ src/main/java/com/semicolon/africa/Messaging/System/services/MailboxesServiceImpl.java
@@ -0,0 +1,27 @@
+package com.semicolon.africa.Messaging.System.services;
+
+import com.semicolon.africa.Messaging.System.data.models.Mailbox;
+import com.semicolon.africa.Messaging.System.data.models.MailboxType;
+import com.semicolon.africa.Messaging.System.data.models.Mailboxes;
+import com.semicolon.africa.Messaging.System.data.repositories.MailboxesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MailboxesServiceImpl implements MailboxesService{
+
+    @Autowired
+    private MailboxesRepository mailboxesRepository;
+    @Override
+    public Mailboxes createMailbox(String email, Mailbox mailbox) {
+
+        Mailboxes mailboxes = new Mailboxes();
+        mailbox.setMailboxType(MailboxType.INBOX);
+        mailboxes.getMailboxes().add(mailbox);
+
+        mailbox.setMailboxType(MailboxType.SENT);
+        mailboxes.getMailboxes().add(mailbox);
+
+        mailboxesRepository.save(mailboxes);
+
+        return mailboxes;
+    }
+}
