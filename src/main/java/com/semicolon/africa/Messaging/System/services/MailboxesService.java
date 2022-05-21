4c54081c29b31037eb8d46a03ad0b4f5c1a28a2c
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-21 12:40:17 +0100

    new commit

---
+++ src/main/java/com/semicolon/africa/Messaging/System/services/MailboxesService.java
@@ -0,0 +1,9 @@
+package com.semicolon.africa.Messaging.System.services;
+
+import com.semicolon.africa.Messaging.System.data.models.Mailbox;
+import com.semicolon.africa.Messaging.System.data.models.Mailboxes;
+
+public interface MailboxesService {
+
+     Mailboxes createMailbox(String email, Mailbox Mailbox);
+}
