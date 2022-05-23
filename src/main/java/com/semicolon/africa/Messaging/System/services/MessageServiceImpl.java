35406eed66973e1c5ee7f13ba6e97f1e158096da
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-23 22:49:21 +0100

    new commit

---
+++ src/main/java/com/semicolon/africa/Messaging/System/services/MessageServiceImpl.java
@@ -0,0 +1,23 @@
+package com.semicolon.africa.Messaging.System.services;
+
+import com.semicolon.africa.Messaging.System.data.models.Message;
+import com.semicolon.africa.Messaging.System.data.repositories.MessageRepository;
+import com.semicolon.africa.Messaging.System.dtos.request.CreateMessageDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MessageServiceImpl implements MessageService{
+
+    @Autowired
+    private MessageRepository messageRepository;
+    @Override
+    public Message sendMessage(CreateMessageDto createMessageDto) {
+
+        Message newMessage = Message.builder()
+                .sender(createMessageDto.getSender())
+                .receiver(createMessageDto.getReceiver())
+                .localDateTime(createMessageDto.getLocalDateTime())
+                .body(createMessageDto.getBody())
+                .build();
+        return messageRepository.save(newMessage);
+    }
+}
