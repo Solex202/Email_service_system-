52ed1422175299b18ae77c463b6fcf2171dc9865
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-23 23:27:35 +0100

    new commit

--- src/main/java/com/semicolon/africa/Messaging/System/data/repositories/UserRepository.java
+++ src/main/java/com/semicolon/africa/Messaging/System/data/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import com.semicolon.africa.Messaging.System.data.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    User findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/com/semicolon/africa/Messaging/System/services/MessageServiceImpl.java
+++ src/main/java/com/semicolon/africa/Messaging/System/services/MessageServiceImpl.java
@@ -1,8 +1,12 @@
 package com.semicolon.africa.Messaging.System.services;
 
 import com.semicolon.africa.Messaging.System.data.models.Message;
+import com.semicolon.africa.Messaging.System.data.models.Notification;
+import com.semicolon.africa.Messaging.System.data.models.User;
 import com.semicolon.africa.Messaging.System.data.repositories.MessageRepository;
+import com.semicolon.africa.Messaging.System.data.repositories.UserRepository;
 import com.semicolon.africa.Messaging.System.dtos.request.CreateMessageDto;
+import com.semicolon.africa.Messaging.System.exception.UserDoesNotExistException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +16,9 @@ public class MessageServiceImpl implements MessageService{
 
     @Autowired
     private MessageRepository messageRepository;
+
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public Message sendMessage(CreateMessageDto createMessageDto) {
 
@@ -21,6 +28,17 @@ public class MessageServiceImpl implements MessageService{
                 .localDateTime(createMessageDto.getLocalDateTime())
                 .body(createMessageDto.getBody())
                 .build();
+
+        User recipient = userRepository.findByEmail(createMessageDto.getReceiver()).orElseThrow(() -> new UserDoesNotExistException("receiver not founder"));
+        Notification notification = Notification.builder()
+                .message(createMessageDto.getBody())
+                .email(createMessageDto.getSender())
+                .title("new message alert ")
+                .id(newMessage.getMsgId())
+                .build();
+        recipient.getNotifications().add(notification);
+        userRepository.save(recipient);
+
         return messageRepository.save(newMessage);
     }
 }
