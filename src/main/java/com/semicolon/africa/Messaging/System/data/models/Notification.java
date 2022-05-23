a7d2c87182977642b9e72c270dbf11987259c468
Onwuka Lotachukwu <christiansolomon@gmail.com>
2022-05-23 23:11:33 +0100

    new commit

---
+++ src/main/java/com/semicolon/africa/Messaging/System/data/models/Notification.java
@@ -0,0 +1,19 @@
+package com.semicolon.africa.Messaging.System.data.models;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Notification {
+
+    private String id;
+    private String title;
+    private  String email;
+    private String message;
+}
